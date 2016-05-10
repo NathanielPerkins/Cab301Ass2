9a5154875a892020e454d793a16e66ec7f79742b
NathanielPerkins <thallazar@gmail.com>
2016-05-10 13:47:43 +1000

    Initial Project and documentation

---
+++ Code/Eclipse/Debug/src/MedianFinder.d
@@ -0,0 +1,1 @@
+src/MedianFinder.d: ../src/MedianFinder.cpp
---
+++ Code/Eclipse/src/MedianFinder.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : MedianFinder.cpp
+// Author      : Nathan Perkins & Lachlan Cesca
+// Version     :
+// Copyright   : No Copyright
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
